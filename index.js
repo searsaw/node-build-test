4db90e0b43cd2025dbb8836b2e86d165df2cf40c
Alex Sears <alex.sears@tangramflex.com>
2018-11-15 11:29:04 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import http from 'http';
+import express, { Router } from 'express';
+
+const app = express();
+const router = Router();
+
+router.get('/foo', (req, res) => {
+  res.json({ msg: 'You have hit the foo endpoint' });
+});
+
+router.get('/bar',(req,res) => {
+  res.json({ msg: 'You have hit the foo endpoint' });
+});
+
+app.use(router)
+
+const server = http.createServer(app);
+
+server.listen('8000', () => {
+  console.log('The server is listening on port 8000');
+});
